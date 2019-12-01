e2497da3aa042cae248b7090e6895974503d92c8
Sin Shimozono <una.veritas@me.com>
2019-12-01 13:46:16 +0900

    MIDIFileLister.cpp

--- MIDIFileLister/MIDIFileLister.cpp
+++ MIDIFileLister/MIDIFileLister.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 #include <cstdlib>
-#include <stack>
+#include <deque>
 #include <regex>
 #include <dirent.h>
 
 struct DirPathScanner {
 	typedef std::pair<std::string,DIR*> pathdir;
-	std::stack<pathdir> pathdirs;
+	std::deque<pathdir> pathdirs;
 	dirent * lastentry;
-	std::regex pattern;
 /*
 #define DT_UNKNOWN       0
 #define DT_FIFO          1
@@ -21,51 +20,59 @@ struct DirPathScanner {
 #define DT_WHT          14
 */
 
-	DirPathScanner(const char * path, const char * pat = ".*") : pathdirs(), lastentry(NULL), pattern(pat) {
-		DIR * dp = opendir(path);
-		if ( dp == NULL ) {
+	DirPathScanner(const char *path) : pathdirs(), lastentry(NULL) {
+		std::string rootpath(path);
+		if ( rootpath.size() != 0 and rootpath[rootpath.size()-1] == '/' )
+			rootpath.pop_back();
+		DIR *dp = opendir(rootpath.c_str());
+		if (dp == NULL) {
 			std::cerr << "error: opendir returned a NULL pointer for the path "
 					<< path << std::endl;
 			return;
 		}
-		pathdirs.push(pathdir(std::string(path),dp));
+		pathdirs.push_back(pathdir(rootpath, dp));
 	}
 
 	bool operator()() const {
 		return !pathdirs.empty();
 	}
 
-	dirent * get_next_regular() {
-		std::string subdirname;
+	const std::string & rootpath() const {
+		return pathdirs.front().first;
+	}
+
+	dirent * get_next_file(const char * regpat = ".*") {
+		std::regex fpattern(regpat);
+		std::string subdir;
 		for(;;) {
-			lastentry = readdir(pathdirs.top().second);
+			lastentry = readdir(pathdirs.back().second);
 			if ( lastentry == NULL ) {
-				pathdirs.pop();
+				pathdirs.pop_back();
 				if ( pathdirs.empty() )
 					break;
 				continue;
-			} else if ( is_directory() ) {
+			} else if ( entry_isdir() ) {
 				//std::cout << pathdirs.top().first << " " << lastentry->d_name << std::endl;
-				if ( strcmp(node_name(), ".") == 0 )
+				if ( strcmp(entry_name(), ".") == 0 )
 					continue;
-				if ( strcmp(node_name(), "..") == 0 )
+				if ( strcmp(entry_name(), "..") == 0 )
 					continue;
-				subdirname = pathdirs.top().first;
-				if ( subdirname[subdirname.size()-1] != '/' ) {
-					subdirname += "/";
-				}
-				subdirname += node_name();
-				DIR * dp = opendir(subdirname.c_str());
+				subdir = "/";
+				subdir += entry_name();
+				if ( pathdirs.back() != pathdirs.front() )
+					subdir = pathdirs.back().first + subdir;
+				DIR * dp = opendir((rootpath()+subdir).c_str());
+				//std::cerr << (rootpath()+subdir) << std::endl;
 				if ( dp == NULL ) {
 					std::cerr << "error: opendir returned a NULL pointer for the path "
-							<< subdirname << std::endl;
+							<< subdir << std::endl;
 					break;
 				} else {
-					pathdirs.push(pathdir(subdirname, dp));
+					pathdirs.push_back(pathdir(subdir, dp));
 					continue;
 				}
-			} else if ( is_regular() ) {
-				if ( std::regex_search(node_name(), pattern) ) {
+			} else if ( entry_isreg() ) {
+				if ( std::regex_match(entry_name(), fpattern) ) {
 					break;
 				}
 				continue;
@@ -77,30 +84,38 @@ struct DirPathScanner {
 		return lastentry;
 	}
 
-	int node_type() const {
+	int entry_type() const {
 		if ( lastentry == NULL )
 			return DT_UNKNOWN;
 		return lastentry->d_type;
 	}
 
-	bool is_directory() const {
-		return node_type() == DT_DIR;
+	bool entry_isdir() const {
+		return entry_type() == DT_DIR;
 	}
 
-	bool is_regular() const {
-		return node_type() == DT_REG;
+	bool entry_isreg() const {
+		return entry_type() == DT_REG;
 	}
 
-	const char * node_name() const {
+	const char * entry_name() const {
 		if ( lastentry == NULL )
 			return (const char * ) NULL;
 		return lastentry->d_name;
 	}
 
-	const char * node_path() const {
+	const char * entry_path() const {
 		if ( lastentry == NULL )
 			return (const char * ) NULL;
-		return pathdirs.top().first.c_str();
+		if (pathdirs.front() == pathdirs.back())
+			return "";
+		return pathdirs.back().first.c_str();
+	}
+
+	const char * entry_fullpath() const {
+		if ( lastentry == NULL )
+			return (const char * ) NULL;
+		return (pathdirs.front().first + pathdirs.back().first).c_str();
 	}
 };
 
@@ -110,16 +125,24 @@ int main(const int argc, const char *argv[]) {
 		exit(1);
 	}
 	const char *basepath = argv[1];
-	DirPathScanner dpscanner(basepath, ".*\\.mid");
+	const char dontcare[] = ".*";
+	const char * p;
+	if ( argc == 3 ) {
+		p = argv[2];
+	} else {
+		p = dontcare;
+	}
+
+	DirPathScanner dpscanner(basepath);
 
 	int counter = 0;
 	if ( ! dpscanner() ) {
 		std::cerr << "error: opendir returned a NULL pointer." << std::endl;
 		exit(1);
 	}
-	while (dpscanner.get_next_regular() != NULL) {
+	while (dpscanner.get_next_file(p) != NULL) {
 		++counter;
-		std::cout << counter << ": " << dpscanner.node_path() << "/" << dpscanner.node_name() << std::endl;
+		std::cout << counter << ": " << dpscanner.entry_path() << "/" << dpscanner.entry_name() << std::endl;
 	}
 	return 0;
 }
