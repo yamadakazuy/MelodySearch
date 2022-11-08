1eb22c6ea01ff1ea1193269f3ffde51e9a9c2ac0
Sin Shimozono <una.veritas@me.com>
2022-11-09 01:24:41 +0900

    mynfa

--- MyNFA/src/MyNFA.cpp
+++ MyNFA/src/MyNFA.cpp
@@ -61,6 +61,8 @@ char * bset64_str(bset64 bits, char * buf) {
 #define STATE_IS_NOT_FINAL 		0
 #define STATE_IS_FINAL 			1
 
+#define UINT64(x,i) (uint64_t(x)<<(i))
+
 /* 文字列から nfa を初期化 */
 void nfa_define(nfa * mp, string melody, int initial, int finals) {
 	int statesize = melody.length();
@@ -78,38 +80,42 @@ void nfa_define(nfa * mp, string melody, int initial, int finals) {
 		char c = melody[i];
 
 		if(i == 0){
-			mp->delta[i][(int)'+'] |= 1<<(i);
-			mp->delta[i][(int)'#'] |= 1<<(i);
-			mp->delta[i][(int)'-'] |= 1<<(i);
-			mp->delta[i][(int)'b'] |= 1<<(i);
-			mp->delta[i][(int)'='] |= 1<<(i);
-			mp->delta[i][(int)'*'] |= 1<<(i);
+			mp->delta[i][(int)'+'] |= UINT64(1,i);
+			mp->delta[i][(int)'#'] |= UINT64(1,i);
+			mp->delta[i][(int)'-'] |= UINT64(1,i);
+			mp->delta[i][(int)'b'] |= UINT64(1,i);
+			mp->delta[i][(int)'='] |= UINT64(1,i);
+			mp->delta[i][(int)'*'] |= UINT64(1,i);
 		}
 
 		if(i == statesize){
-			mp->delta[i][(int)'+'] |= 1<<(i);
-			mp->delta[i][(int)'#'] |= 1<<(i);
-			mp->delta[i][(int)'-'] |= 1<<(i);
-			mp->delta[i][(int)'b'] |= 1<<(i);
-			mp->delta[i][(int)'='] |= 1<<(i);
+			mp->delta[i][(int)'+'] |= UINT64(1,i);
+			mp->delta[i][(int)'#'] |= UINT64(1,i);
+			mp->delta[i][(int)'-'] |= UINT64(1,i);
+			mp->delta[i][(int)'b'] |= UINT64(1,i);
+			mp->delta[i][(int)'='] |= UINT64(1,i);
 		}
 
 		if(c == '+'){
-			mp->delta[i][(int)'+'] |= 1<<(i+1);
+			mp->delta[i][(int)'+'] |= UINT64(1, i+1);
 		}else if(c == '#'){
-			mp->delta[i][(int)'#'] |= 1<<(i+1);
+			mp->delta[i][(int)'#'] |= UINT64(1, i+1);
 		}else if(c == '^'){
-			mp->delta[i][(int)'+'] |= 1<<(i+1);
-			mp->delta[i][(int)'#'] |= 1<<(i+1);
+			mp->delta[i][(int)'+'] |= UINT64(1, i+1);
+			mp->delta[i][(int)'#'] |= UINT64(1, i+1);
 		}else if(c == '-'){
-			mp->delta[i][(int)'-'] |= 1<<(i+1);
+			mp->delta[i][(int)'-'] |= UINT64(1, i+1);
 		}else if(c == 'b'){
-			mp->delta[i][(int)'b'] |= 1<<(i+1);
+			mp->delta[i][(int)'b'] |= UINT64(1, i+1);
 		}else if(c == '_'){
-			mp->delta[i][(int)'-'] |= 1<<(i+1);
-			mp->delta[i][(int)'b'] |= 1<<(i+1);
+			mp->delta[i][(int)'-'] |= UINT64(1, i+1);
+			mp->delta[i][(int)'b'] |= UINT64(1, i+1);
 		}else if(c == '='){
-			mp->delta[i][(int)'='] |= 1<<(i+1);
+			mp->delta[i][(int)'='] |= UINT64(1, i+1);
+		}
+		for(int a = 0; a < ALPHABET_LIMIT; ++a) {
+			if ( mp->delta[i][(int)a] )
+				std::cout << "(" << i << ", " << (char) a << ", " << mp->delta[i][(int)a] << ")" << endl;
 		}
 	}
 	mp->initial = initial;
@@ -127,9 +133,10 @@ void nfa_print(nfa * mp) {
 	}
 	for(int i = 0; i < STATE_LIMIT; ++i) {
 		for(int a = 0; a < ALPHABET_LIMIT; ++a) {
-			if ( mp->delta[i][a] != 0) {
-				states |= 1<<i;
-				states |= (int)mp->delta[i][a];
+			if ( mp->delta[i][a] ) {
+				std::cout << "(" << i << ", " << (char) a << ", " << mp->delta[i][a] << ")" << endl;
+				states |= UINT64(1, i);
+				states |= mp->delta[i][a];
 				alphabet[a] = true;
 				//std::cout << (char) a << " " << (int) alphabet[a] << ", ";
 			}
