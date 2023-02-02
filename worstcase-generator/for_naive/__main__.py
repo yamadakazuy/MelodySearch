00da1411ad5a721a723a8c44395be9b1adeee0e2
Sin Shimozono <una.veritas@icloud.com>
2023-02-02 15:41:10 +0900

    worstcase generator

---
+++ worstcase-generator/for_naive/__main__.py
@@ -0,0 +1,30 @@
+'''
+Created on 2023/02/02
+
+@author: sin
+'''
+
+import sys
+
+if __name__ == '__main__':
+    if (len(sys.argv) == 1) :
+        print("usage: command [length] [output file name]")
+        exit(0)
+    
+    if (len(sys.argv) <= 1) :
+        outlength = 1024
+    else:
+        outlength = max(1024,int(sys.argv[1]))
+    if (len(sys.argv) <= 2) :
+        outfilename = "output.cont"
+    else:
+        outfilename = sys.argv[2]
+    
+    print("output file length = " + str(outlength))
+    
+    with open(outfilename, "w") as of :
+        of.write("0")
+        for i in range(outlength - 1) :
+            of.write("=")
+    
+    
